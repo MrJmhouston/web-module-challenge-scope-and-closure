dfa499a067fd209a66dbabfa6af5441060e34aa0
Julian <67443300+MrJmhouston@users.noreply.github.com>
2020-08-17 16:20:47 -0400

    complete

--- index.js
+++ index.js
@@ -28,10 +28,18 @@ function processFirstItem(stringList, callback) {
  * 
  * 1. What is the difference between counter1 and counter2?
  * 
+ * counter1 has set the variable equal to the counterMaker function, while counter2 has skipped this step.
+ * ----------------------------------------------------------
  * 2. Which of the two uses a closure? How can you tell?
+ * 
+ * counter1 uses a closure, I can tell because closures give us the ability too put functions together.
+ * 
+ * ----------------------------------------------------------
  * 
  * 3. In what scenario would the counter1 code be preferable? In what scenario would counter2 be better? 
  *
+ * counter1 would be useful if we needed to access the function again even after the parent funtion has been terminated, counter2 would be preferred if we do not want to access that function again.
+ * ----------------------------------------------------------
 */
 
 // counter1 code
@@ -56,11 +64,36 @@ function counter2() {
 
 Write a function called `inning` that returns a random number of points that a team scored in an inning. This should be a whole number between 0 and 2. */
 
-function inning(/*Code Here*/){
+function inning(b){
+  let homeScore = Math.floor(Math.random() * (2 - 0 + 1)) + 0;
+  let awayScore = Math.floor(Math.random() * (2 - 0 + 1)) + 0;
+  let homeTeam = "Home team";
+  let awayTeam = "Away team";
 
-    /*Code Here*/
-
+  let inning = b;
+  for (i = 0; i < inning.length; i++) {
+    return inning;
+  }
+  if (inning === 9 && homeScore > 1) {
+    return `The final score is ${homeScore} runs for the ${homeTeam} and ${awayScore} runs for the ${awayTeam} in the ${inning}th inning.`;
+  } else if (inning === 9 && homeScore === 0) {
+    return `The final score is ${homeScore} runs for the ${homeTeam} and ${awayScore} runs for the ${awayTeam} in the ${inning}th inning.`;
+  } else if (inning === 9 && homeScore === 1){
+    return `The final score is ${homeScore} run for the ${homeTeam} and ${awayScore} run for the ${awayTeam} in the ${inning}th inning.`;
+  } else if (inning === 1) {
+    return `The current score is ${homeScore} run for the ${homeTeam} and ${awayScore} run for the ${awayTeam} in the ${inning}st inning.`;
+  } else if (inning === 2) {
+    return `The current score is ${homeScore} runs for the ${homeTeam} and ${awayScore} runs for the ${awayTeam} in the ${inning}nd inning.`;
+  } else if (inning === 3) {
+    return `The current score is ${homeScore} runs for the ${homeTeam} and ${awayScore} runs for the ${awayTeam} in the ${inning}rd inning.`;
+  } else if (inning >= 4 && inning <= 8) {
+    return `The current score is ${homeScore} runs for the ${homeTeam} and ${awayScore} runs for the ${awayTeam} in the ${inning}th inning.`;
+  } else {
+    return `The current score is ${homeScore} runs for the ${homeTeam} and ${awayScore} runs for the ${awayTeam} in the ${inning} inning.`;
+  }
 }
+
+console.log(inning(9));
 
 /* Task 3: finalScore()
 
@@ -76,11 +109,12 @@ finalScore(inning, 9) might return:
 
 */ 
 
-function finalScore(/*code Here*/){
-
-  /*Code Here*/
+function finalScore(cb, num){
+return cb(num);
 
 }
+
+console.log(finalScore(inning, 9));
 
 /* Task 4: 
 
@@ -90,7 +124,7 @@ Create a function called `scoreboard` that accepts the following parameters:
 (2) Callback function `inning`
 (3) A number of innings
 
-and returns the score at each pont in the game, like so:
+and returns the score at each point in the game, like so:
 1st inning: awayTeam - homeTeam
 2nd inning: awayTeam - homeTeam
 3rd inning: awayTeam - homeTeam
@@ -103,8 +137,24 @@ and returns the score at each pont in the game, like so:
 Final Score: awayTeam - homeTeam */
 
 
-function scoreboard(/* CODE HERE */) {
-  /* CODE HERE */
+function scoreboard(cb, cb2, num) {
+  let inning = 1;
+  return function newInning() {
+    inning++;
+  }
+  return cb2(num);
 }
+
+console.log(inning(1));
+console.log(inning(2));
+console.log(inning(3));
+console.log(inning(4));
+console.log(inning(5));
+console.log(inning(6));
+console.log(inning(7));
+console.log(inning(8));
+console.log(inning(9));
+
+
 
 
